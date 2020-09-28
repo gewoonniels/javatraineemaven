b2c8727ee75a854ab7c00be1dc41d782462c4749
Niels Vredeveldt <niels.vredeveldt@live.nl>
2020-09-28 13:11:21 +0200

    modified annotations

--- src/test/java/traject/TrajectPrijsServiceTest.java
+++ src/test/java/traject/TrajectPrijsServiceTest.java
@@ -2,7 +2,10 @@ package traject;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -10,28 +13,28 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TrajectPrijsServiceTest {
 
+    @InjectMocks
     private TrajectPrijsService target;
+
+    @Mock
+    private TrajectEenhedenNaarPrijsService naarPrijs;
+
+    @Mock
+    private TrajectNaarTrajectEenhedenService naarEenheden;
+
 
     @BeforeEach
     void setUp() {
-        target = new TrajectPrijsService();
+        when(naarEenheden.getTrajectEenheden(anyString(), anyString())).thenReturn(0);
+        when(naarPrijs.getPriceTrajectEenheden(anyInt())).thenReturn(33);
     }
 
     @Test
     void getTracjectPrijs() {
-        TrajectNaarTrajectEenhedenService naarEenheden = Mockito.mock(TrajectNaarTrajectEenhedenService.class);
-        TrajectEenhedenNaarPrijsService naarPrijs = Mockito.mock(TrajectEenhedenNaarPrijsService.class);
-
-        when(naarEenheden.getTrajectEenheden(anyString(), anyString())).thenReturn(0);
-        when(naarPrijs.getPriceTrajectEenheden(anyInt())).thenReturn(33);
-
-        target.setTrajectNaarTrajectEenheden(naarEenheden);
-        target.setEenhedenNaarPrijs(naarPrijs);
-
         int prijs = target.getTracjectPrijs("utrecht", "amsterdam");
         assertThat(prijs, is(33));
-
     }
 }
